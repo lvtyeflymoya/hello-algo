0737c52064b514c66b0ff63a5b88f78974a36905
lvtyeflymoya <Zhanglele0526@126.com>
2025-03-22 16:46:36 +0800

    完成树和堆

--- exercise/cpp/chapter_heap/my_heap.cpp
+++ exercise/cpp/chapter_heap/my_heap.cpp
@@ -1,66 +1,53 @@
-/**
- * File: my_heap.cpp
- * Created Time: 2023-02-04
- * Author: LoneRanger (836253168@qq.com), what-is-me (whatisme@outlook.jp)
- */
-
 #include "../utils/common.hpp"
+#include <algorithm>
 
-/* 大顶堆 */
-class MaxHeap {
-  private:
-    // 使用动态数组，这样无须考虑扩容问题
+class MaxHeap{
+private:
     vector<int> maxHeap;
 
-    /* 获取左子节点的索引 */
-    int left(int i) {
-        return 2 * i + 1;
+    int left(int index){
+        return 2 * index + 1;
     }
 
-    /* 获取右子节点的索引 */
-    int right(int i) {
-        return 2 * i + 2;
+    int right(int index){
+        return 2 * index + 2;
     }
 
-    /* 获取父节点的索引 */
-    int parent(int i) {
-        return (i - 1) / 2; // 向下整除
+    int parent(int index){
+        return (index - 1) / 2;
     }
 
+    
     /* 从节点 i 开始，从底至顶堆化 */
-    void siftUp(int i) {
-        while (true) {
-            // 获取节点 i 的父节点
-            int p = parent(i);
-            // 当“越过根节点”或“节点无须修复”时，结束堆化
-            if (p < 0 || maxHeap[i] <= maxHeap[p])
+    void siftUp(int idx){
+        while(true){
+            int p = parent(idx);
+            if(p < 0 || maxHeap[idx] <= maxHeap[p])
                 break;
-            // 交换两节点
-            swap(maxHeap[i], maxHeap[p]);
-            // 循环向上堆化
-            i = p;
+            swap(maxHeap[idx], maxHeap[p]);
+            idx = p;
         }
     }
 
     /* 从节点 i 开始，从顶至底堆化 */
-    void siftDown(int i) {
-        while (true) {
-            // 判断节点 i, l, r 中值最大的节点，记为 ma
+    void siftDown(int i){
+        while (true)
+        {
             int l = left(i), r = right(i), ma = i;
             if (l < size() && maxHeap[l] > maxHeap[ma])
                 ma = l;
             if (r < size() && maxHeap[r] > maxHeap[ma])
                 ma = r;
-            // 若节点 i 最大或索引 l, r 越界，则无须继续堆化，跳出
-            if (ma == i)
+            if (ma = i)
                 break;
             swap(maxHeap[i], maxHeap[ma]);
-            // 循环向下堆化
             i = ma;
         }
+        
     }
 
-  public:
+    
+public:
     /* 构造方法，根据输入列表建堆 */
     MaxHeap(vector<int> nums) {
         // 将列表元素原封不动添加进堆
@@ -71,8 +58,8 @@ class MaxHeap {
         }
     }
 
-    /* 获取堆大小 */
-    int size() {
+    // 修改返回类型为 size_t
+    int size(){
         return maxHeap.size();
     }
 
@@ -81,32 +68,28 @@ class MaxHeap {
         return size() == 0;
     }
 
-    /* 访问堆顶元素 */
-    int peek() {
+    int peek(){
         return maxHeap[0];
     }
 
-    /* 元素入堆 */
-    void push(int val) {
-        // 添加节点
-        maxHeap.push_back(val);
-        // 从底至顶堆化
+    void push(int num){
+        maxHeap.push_back(num);
         siftUp(size() - 1);
     }
 
+   
+
     /* 元素出堆 */
-    void pop() {
+    int pop(){
         // 判空处理
-        if (isEmpty()) {
+        if (maxHeap.empty()){
             throw out_of_range("堆为空");
         }
-        // 交换根节点与最右叶节点（交换首元素与尾元素）
         swap(maxHeap[0], maxHeap[size() - 1]);
-        // 删除节点
         maxHeap.pop_back();
-        // 从顶至底堆化
         siftDown(0);
     }
+
 
     /* 打印堆（二叉树）*/
     void print() {
@@ -118,7 +101,6 @@ class MaxHeap {
         freeMemoryTree(root);
     }
 };
-
 /* Driver Code */
 int main() {
     /* 初始化大顶堆 */
@@ -153,6 +135,4 @@ int main() {
 
     return 0;
 }
-
-
 
